b8758617251ed96918f161c6e4e9da70fa4a919f
Gennadiy <renardes@gmail.com>
2022-09-06 08:21:18 +0500

    08.31: правки удобства чтения

--- 08.31/1.py
+++ 08.31/1.py
@@ -1,6 +1,7 @@
 from math import sqrt
 from typing import Iterable
 
+
 def my_func(func: Iterable, 
             *args: int | float, 
             key: bool = True) -> str | float:
@@ -10,15 +11,41 @@ def my_func(func: Iterable,
     result = func(*args)
     return str(round(result, 2)) if key else float(round(result, 2))
 
-print(my_func(lambda k, x, b: k * x + b, 10, 12, 5))
-print(my_func(lambda a, b, c, x: a * x**2 + b * x + c, 10.1, 12.1, 6.6, 5.01))
-print(my_func(lambda a, b, c: (((a + b + c) ** 3)) ** (1/2), 10, 12, 18))
-print(my_func(lambda a, b, c: (((a + b + c) ** 3)) ** (1/2), 1, 2, 8, key=False))
-print(my_func(lambda a, b: (a * b) / (a + b), 10, 12, key=False))
-print(type(my_func(lambda a, b: (a * b) / (a + b), 10, 12, key=False)))
-print(type(my_func(lambda a, b: (a * b) / (a + b), 10, 12)))
 
-#stdout:
+print(my_func(
+    lambda k, x, b: k * x + b,
+    10, 12, 5
+))
+print(my_func(
+    lambda a, b, c, x: a * x**2 + b * x + c,
+    10.1, 12.1, 6.6, 5.01
+))
+print(my_func(
+    lambda a, b, c: ((a + b + c)**3)**(1/2),
+    10, 12, 18
+))
+print(my_func(
+    lambda a, b, c: ((a + b + c)**3)**(1/2),
+    1, 2, 8,
+    key=False
+))
+print(my_func(
+    lambda a, b: (a * b) / (a + b),
+    10, 12,
+    key=False))
+
+print(type(my_func(
+    lambda a, b: (a * b) / (a + b),
+    10, 12,
+    key=False
+)))
+print(type(my_func(
+    lambda a, b: (a * b) / (a + b),
+    10, 12
+)))
+
+
+# stdout:
 # 125
 # 320.73
 # 252.98
