3698afb03a354bc0e844ed8d97c297daed299e48
Gennadiy <renardes@gmail.com>
2022-09-21 13:07:05 +0500

    09.12: комментарии, исправления, итоги

--- 09.12/1.py
+++ 09.12/1.py
@@ -1,8 +1,9 @@
-br_open, br_close = ('([{'), (')]}')
+# ИСПОЛЬЗОВАТЬ: строковые литералы окружаются скобками только в том случае, если нам необходимо перенести литерал на другую строку в коде
+br_open, br_close = '([{', ')]}'
 br_trans = str.maketrans(br_close, br_open)
 
 def check_lisp_line(line: str) -> bool:
-    """Проверяет корректность синтаксиса переданной строки кода на языке LISP и выводит True, либо False."""
+    """Проверяет корректность синтаксиса переданной строки кода на языке LISP и возвращает True, либо False."""
     br_tmp = []
     for char in line:
         if char in br_open:
@@ -12,18 +13,24 @@ def check_lisp_line(line: str) -> bool:
                 return False
             br_pair = char.translate(br_trans)
             if br_pair == br_tmp[-1]:
+                # ОТВЕТИТЬ: сами придумали стек использовать или нашли?
                 br_tmp = br_tmp[:-1]
             else:
                 return False
     return True if not br_tmp else False
-        
+
+
 print(check_lisp_line('(for-loop {[i 0 (< i 10) (inc i)]} (println i))'))
-print(check_lisp_line('((if [a >) 1] println var)'))        
-print(check_lisp_line('(for-loop {[i 0 (< i 10)} [(inc i)]} (println i))'))        
-print(check_lisp_line('((if [a > 1] println var))'))        
+print(check_lisp_line('((if [a >) 1] println var)'))
+print(check_lisp_line('(for-loop {[i 0 (< i 10)} [(inc i)]} (println i))'))
+print(check_lisp_line('((if [a > 1] println var))'))
+
 
 # stdout:
 # True
 # False
 # False
 # True
+
+
+# ИТОГ: отлично! — 12/12
