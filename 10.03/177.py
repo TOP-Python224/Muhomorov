64570de8141697d49e2f10c5311c0ff6dfd8b12e
Gennadiy <renardes@gmail.com>
2022-11-23 18:28:24 +0500

    10.03: правки удобства чтения кода

--- 10.03/177.py
+++ 10.03/177.py
@@ -1,6 +1,6 @@
-rim_to_arab = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1 }
-'XLIII'
-rim_input = input('Введите римское число: ')
+rim_to_arab = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+
+
 
 def from_rim_to_arab(rim_num: str) -> int:
     """Конвертирует полученное римское число в арабское число и выводит его."""
@@ -9,18 +9,21 @@ def from_rim_to_arab(rim_num: str) -> int:
     if len_rim == 0:
         return arab_num
     elif len_rim == 1:
-        arab_num += rim_to_arab[rim_num] 
+        arab_num += rim_to_arab[rim_num]
         return arab_num
     else:
         first, second = rim_to_arab[rim_num[0]], rim_to_arab[rim_num[1]]
-        if first < second: 
+        if first < second:
             arab_num += second - first + from_rim_to_arab(rim_num[2:])
             return arab_num
         else:
             arab_num += first + from_rim_to_arab(rim_num[1:])
             return arab_num
-        
+
+
+rim_input = input('Введите римское число: ')
 print(from_rim_to_arab(rim_input))
+
 
 # stdout: 
 # Введите римское число: XIX
--- 10.03/181.py
+++ 10.03/181.py
@@ -1,9 +1,8 @@
 coins = (25, 10, 5, 1)
-money = int(input('Введите сумму в центах: '))
-amount = int(input('Введите количество монет: '))
+
 
 def check_coins(money, amount, coins) -> bool:
-    """Проверяет возможно ли составить введенную сумму из введенного количества монет номиналом 25, 10, 5, 1 цнетов в любом """
+    """Проверяет возможно ли составить введенную сумму из введенного количества монет номиналом 25, 10, 5, 1 центов в любом """
     quotient, remainder = divmod(money, coins[0])
     if remainder == 0:
         if quotient == amount: 
@@ -21,8 +20,12 @@ def check_coins(money, amount, coins) -> bool:
                 amount -= quotient
                 coins = coins[1:]
                 return check_coins(money, amount, coins)
-    
+
+
+money = int(input('Введите сумму в центах: '))
+amount = int(input('Введите количество монет: '))
 print(check_coins(money, amount, coins))
+
 
 # stdout:
 # Введите сумму в центах: 3
