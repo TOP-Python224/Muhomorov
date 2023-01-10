9d4d48deeb77026afecdf840235115730066ba29
io25nsk <io@sakhanet.ru>
2023-01-10 19:59:48 +0700

    ДЗ за 12.26

---
+++ 12.26/1.1.py
@@ -0,0 +1,144 @@
+"""У вас есть класс, в котором находится несколько методов, вызов которых вы хотите контролировать.
+
+Спроектируйте и реализуйте модель классов по шаблону Команда, обеспечивающих следующую функциональность:
+    1) хранение истории операций
+    2) отмена последних операций
+    3) повторного выполнения отменённых операций
+
+Продемонстрируйте на тестовом классе ниже:"""
+
+from random import randrange as rr, choice as ch
+from string import ascii_lowercase as alc
+from datetime import datetime as dt
+
+
+class TestCase:
+    """Адресат."""
+    def __init__(self):
+        self.messages = [
+            ''.join(ch(alc) for _ in range(rr(3, 6)))
+            for _ in range(1000)
+        ]
+        self.numbers = [
+            (rr(10, 100) for _ in range(rr(4, 7)))
+            for _ in range(1000)
+        ]
+        # ... другие поля?
+        self.dict_ = dict(zip((k for k in range(1000)),
+                              (rr(100, 1000) for _ in range(1000))))
+
+    def print_msg(self):
+        msg = self.messages.pop()
+        print(msg)
+        return msg
+
+    def sum_nums(self):
+        nums = self.numbers.pop()
+        print(sum(nums))
+        return nums
+
+    # ... другие методы?
+    def sum_digits(self):
+        item = self.dict_.popitem()
+        print(sum(map(int, str(item[1]))))
+        return item
+
+
+class TestLogger:
+    """Протоколирует все выполняемые команды."""
+    log = []
+
+    @classmethod
+    def append_data(cls, data: str):
+        cls.log.append(data)
+
+
+class TestCommand:
+    """Команда."""
+    def __init__(self, case: TestCase, method: str):
+        self.case = case
+        self.method = method
+        self.pop_list = []
+        self.undo_cnt = 0
+
+    def __log(self, pop_elem: str, action: str) -> None:
+        TestLogger.append_data(f"{dt.now():%Y-%m-%d %H:%M:%S} - {self.method} - {pop_elem} - {action}")
+
+    def execute(self, redo: bool = False) -> None:
+        action = ['POP', 'REDO']
+        match self.method:
+            case 'print_msg':
+                pop_elem = self.case.print_msg()
+            case 'sum_nums':
+                pop_elem = self.case.sum_nums()
+            case 'sum_digits':
+                pop_elem = self.case.sum_digits()
+            case _:
+                raise TypeError('unknown command')
+        self.pop_list.append(pop_elem)
+        self.__log(pop_elem, action[redo])
+
+    def undo(self) -> None:
+        if self.pop_list:
+            pop_elem = self.pop_list.pop()
+            match self.method:
+                case 'print_msg':
+                    self.case.messages.append(pop_elem)
+                case 'sum_nums':
+                    self.case.numbers.append(pop_elem)
+                case 'sum_digits':
+                    self.case.dict_.update([pop_elem])
+                case _:
+                    raise TypeError('unknown command')
+            self.__log(pop_elem, 'UNDO')
+            self.undo_cnt += 1
+        else:
+            print('nothing to undo')
+
+    def redo(self) -> None:
+        if self.undo_cnt > 0:
+            self.execute(redo=True)
+            self.undo_cnt -= 1
+        else:
+            print('nothing to redo')
+
+
+# test_case1 = TestCase()
+# tc1 = TestCommand(test_case1, 'sum_digits')
+# tc1.execute()
+# tc1.execute()
+# tc1.execute()
+# tc1.redo()
+# tc1.undo()
+# tc1.undo()
+# tc1.undo()
+# tc1.undo()
+# tc1.redo()
+# tc1.redo()
+# tc1.redo()
+# tc1.undo()
+# tc1.undo()
+# tc1.undo()
+# print(*TestLogger.log, sep='\n')
+
+# stdout:
+# 15
+# 14
+# 17
+# nothing to redo
+# nothing to undo
+# 15
+# 14
+# 17
+# 2023-01-07 18:15:59 - sum_digits - (999, 690) - POP
+# 2023-01-07 18:15:59 - sum_digits - (998, 905) - POP
+# 2023-01-07 18:15:59 - sum_digits - (997, 674) - POP
+# 2023-01-07 18:15:59 - sum_digits - (997, 674) - UNDO
+# 2023-01-07 18:15:59 - sum_digits - (998, 905) - UNDO
+# 2023-01-07 18:15:59 - sum_digits - (999, 690) - UNDO
+# 2023-01-07 18:15:59 - sum_digits - (999, 690) - REDO
+# 2023-01-07 18:15:59 - sum_digits - (998, 905) - REDO
+# 2023-01-07 18:15:59 - sum_digits - (997, 674) - REDO
+# 2023-01-07 18:15:59 - sum_digits - (997, 674) - UNDO
+# 2023-01-07 18:15:59 - sum_digits - (998, 905) - UNDO
+# 2023-01-07 18:15:59 - sum_digits - (999, 690) - UNDO
---
+++ 12.26/2.1.py
@@ -0,0 +1,112 @@
+from abc import ABC
+from random import choice
+from typing import Optional
+from typing import Self
+from enum import Enum
+
+
+class Codes(Enum):
+    """Предоставляет справочник кодов ответа вэб-сервера."""
+    OK = '200'
+    MOVED = '301'
+    FORBIDDEN = '403'
+    NOT_FOUND = '404'
+    BAD_GATEWAY = '502'
+
+
+class WebServer:
+    """Возвращает случайно выбранный ответ вэб-сервера с кодом."""
+    def __init__(self):
+        self.answers = [
+            (f"HTTP/1.1 {Codes.OK.value} OK", 'Payload'),
+            (f"HTTP/1.1 {Codes.MOVED.value} Moved Permanently", 'Payload'),
+            (f"HTTP/1.1 {Codes.FORBIDDEN.value} Forbidden", 'Payload'),
+            (f"HTTP/1.1 {Codes.NOT_FOUND.value} Not Found", 'Payload'),
+            (f"HTTP/1.1 {Codes.BAD_GATEWAY.value} Bad Gateway", 'Payload')
+        ]
+
+    def get(self):
+        return choice(self.answers)
+
+
+class CodeChecker(ABC):
+    """Базовый класс обработчиков, являющийся корневым элементом цепочки обработчиков и предоставляющий методы
+    для прохождения по всем элементам цепочки."""
+    def __init__(self, code: tuple[str, str]):
+        self.code = code
+        self.next_checker: Optional[CodeChecker] = None
+
+    def add_checker(self, checker: Self):
+        if self.next_checker is None:
+            self.next_checker = checker
+        else:
+            self.next_checker.add_checker(checker)
+
+    def handle(self):
+        if self.next_checker:
+            self.next_checker.handle()
+
+
+class CodeOkChecker(CodeChecker):
+    """При наличии кода 200 в ответе вэб-сервера, выводит сообщение и прерывает цепочку команд,
+    иначе передает код следующему обработчику в цепочке."""
+    def handle(self):
+        if Codes.OK.value in self.code[0]:
+            print(f"{self.__class__.__name__} - {Codes.OK.value}")
+        else:
+            super().handle()
+
+
+class CodeMovedPermChecker(CodeChecker):
+    """При наличии кода 301 в ответе вэб-сервера, выводит сообщение и прерывает цепочку команд,
+    иначе передает код следующему обработчику в цепочке."""
+
+    def handle(self):
+        if Codes.MOVED.value in self.code[0]:
+            print(f"{self.__class__.__name__} - {Codes.MOVED.value}")
+        else:
+            super().handle()
+
+
+class CodeForbiddenChecker(CodeChecker):
+    """При наличии кода 403 в ответе вэб-сервера, выводит сообщение и прерывает цепочку команд,
+    иначе передает код следующему обработчику в цепочке."""
+    def handle(self):
+        if Codes.FORBIDDEN.value in self.code[0]:
+            print(f"{self.__class__.__name__} - {Codes.FORBIDDEN.value}")
+        else:
+            super().handle()
+
+
+class CodeNotFoundChecker(CodeChecker):
+    """При наличии кода 404 в ответе вэб-сервера, выводит сообщение и прерывает цепочку команд,
+    иначе передает код следующему обработчику в цепочке."""
+    def handle(self):
+        if Codes.NOT_FOUND.value in self.code[0]:
+            print(f"{self.__class__.__name__} - {Codes.NOT_FOUND.value}")
+        else:
+            super().handle()
+
+
+class CodeBadGatewayChecker(CodeChecker):
+    """При наличии кода 502 ответе вэб-сервера, выводит сообщение и прерывает цепочку команд,
+    иначе передает код следующему обработчику в цепочке."""
+    def handle(self):
+        if Codes.BAD_GATEWAY.value in self.code[0]:
+            print(f"{self.__class__.__name__} - {Codes.BAD_GATEWAY.value}")
+        else:
+            super().handle()
+
+
+# ws = WebServer()
+# answer = ws.get()
+# cc = CodeChecker(answer)
+# cc.add_checker(CodeOkChecker(answer))
+# cc.add_checker(CodeMovedPermChecker(answer))
+# cc.add_checker(CodeForbiddenChecker(answer))
+# cc.add_checker(CodeNotFoundChecker(answer))
+# cc.add_checker(CodeBadGatewayChecker(answer))
+# cc.handle()
+
+# stdout:
+# CodeForbiddenChecker - 403
